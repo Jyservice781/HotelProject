d6f1f473f4a0adc38f44039c6be156199b3b40b7
Jyservice781 <gimjuyeong512@gmail.com>
2024-08-09 17:35:47 +0900

    hotelproject

---
+++ Hotel-react/src/component/Banner.js
@@ -0,0 +1,13 @@
+let Banner = () => {
+    return(
+        <div style={{
+            width:'100%',
+            height:'200px',
+            backgroundColor: 'lightblue',
+            textAlign: 'center',
+            lineHeight: '200px',
+        }}>광고자리// 이미지만 매꿀 예정</div>
+    )
+}
+
+export default Banner;
