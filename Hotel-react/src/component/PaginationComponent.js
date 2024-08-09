0d8af5224ec0aa18d6fc00c2db03bddfbb168c5b
JEunseo <jjeseo1010@gmail.com>
2024-08-09 17:49:31 +0900

    Create PaginationComponent.js
    0809

---
+++ Hotel-react/src/component/PaginationComponent.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {Pagination} from "react-bootstrap";
+
+const PaginationComponent = ({page, totalPages, setPage}) => {
+    const moveToPre = () => {
+        if (page > 1) setPage(page - 1)
+    }
+
+    const moveToNext = () => {
+        if (page < totalPages) setPage(page + 1)
+    }
+
+    const moveToFirst = () => {
+        setPage(1)
+    }
+
+    const moveToLast = () => {
+        setPage(totalPages)
+    }
+
+    const pageItem = () => {
+        let items = [];
+        const startPage = Math.max(1, page - 1);
+        const endPage = Math.min(totalPages, page + 1);
+
+        for (let i = startPage; i <= endPage; i++) {
+            items.push(
+                <Pagination.Item key={i} active={i === page} onClick={() => setPage(i)}>
+                    {i}
+                </Pagination.Item>
+            );
+        }
+        return items;
+    };
+
+    return (
+        <Pagination>
+            <Pagination.First onClick={moveToFirst} disabled={page === 1}/>
+            <Pagination.Prev onClick={moveToPre} disabled={page === 1}/>
+            {pageItem()}
+            <Pagination.Next onClick={moveToNext} disabled={page === totalPages}/>
+            <Pagination.Last onClick={moveToLast} disabled={page === totalPages}/>
+        </Pagination>
+    )
+}
+
+export default PaginationComponent
