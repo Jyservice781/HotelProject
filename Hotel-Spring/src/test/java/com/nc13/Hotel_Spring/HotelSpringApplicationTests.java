e692c4b1ae47271a4bdb2b05f7e30eaacafb52a9
Jyservice781 <gimjuyeong512@gmail.com>
2024-08-09 17:39:10 +0900

    hotel-spring

---
+++ Hotel-Spring/src/test/java/com/nc13/Hotel_Spring/HotelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc13.Hotel_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
