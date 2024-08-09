62b14ed75ad8223e6fbcc9b3fb0e5f9dd81e34d2
JEunseo <jjeseo1010@gmail.com>
2024-08-09 17:53:02 +0900

    Create SecurityConfig.java
    0809

---
+++ Hotel-Spring/src/main/java/com/nc13/Hotel_Spring/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.nc13.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public CorsFilter corsFiler(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
