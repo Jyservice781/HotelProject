c7c5dc843b00777a82e77b0013d2b18eeae9d826
Jyservice781 <gimjuyeong512@gmail.com>
2024-08-18 19:57:33 +0900

    push front 03

--- Hotel-react/src/component/Header.js
+++ Hotel-react/src/component/Header.js
@@ -2,17 +2,29 @@ import {Link} from "react-router-dom";
 import {Stack} from "react-bootstrap";
 
 let Header = ({ userInfo }) => {
-    let btnStyle = {
-        listStyleType: 'none',
-        display: 'flex',
+
+    const btnStyle = {
+        padding: '10px 15px',
+        backgroundColor: '#fff',
+        color: '#439798',
+        border: '1px solid #439798',
+        borderRadius: '5px',
+        fontSize: '14px',
+        textDecoration: 'none',
     }
 
     let headerStyle = {
         background: '#ffffff',
-        boxShadow:  '15px 15px 45px #bdbdbd, -15px -15px 45px #ffffff',
+        boxShadow:  '3px 5px 69px #bdbdbd, -15px -15px 45px #ffffff',
         padding: '0px',
         margin: '0px',
-        marginBottom: '50px'
+        marginBottom: '20px'
+    }
+
+    let ulStyle = {
+        listStyleType: 'none',
+        display: 'flex',
+        alignItems: 'center',
     }
 
 
@@ -26,30 +38,30 @@ let Header = ({ userInfo }) => {
                              height:'auto'
                          }}/>
                 </Link>
-                <ul className={'flex ms-auto'} style={btnStyle}>
-                    <li> welcome, {userInfo ? userInfo.nickname : 'Guest'}. </li>
+                <ul className={'flex ms-auto'} style={ulStyle}>
+                    <li><span> welcome, {userInfo ? userInfo.nickname : 'Guest'}. </span></li>
                     {/*사용자 역할에 따라 링크 표시 */}
                     {userInfo ? (
                         <>
                         {userInfo.role === 'role_admin' && (
-                            <li className={'m-lg-3'}><Link to={'/admin/users'}>회원 관리</Link></li>
+                            <li className={'m-lg-3'}><Link to={'/admin/users'} style={btnStyle}>회원 관리</Link></li>
                         )}
                         {userInfo.role === 'role_seller' && (
-                            <li className={'m-lg-3'}><Link to={`/user/myhotel/${userInfo.id}`}>내 호텔 관리</Link></li>
+                            <li className={'m-lg-3'}><Link to={`/user/myhotel/${userInfo.id}`} style={btnStyle}>내 호텔 관리</Link></li>
                         )}
                         {userInfo.role === 'role_customer' && (
-                            <ul>
-                                <li className={'m-lg-3'}><Link to={`/user/mypage/${userInfo.id}`}>마이페이지</Link></li>
-                                <li className={'m-lg-3'}><Link to={`/user/basket/${userInfo.id}`}>장바구니</Link></li>
-                                <li className={'m-lg-3'}><Link to={`/user/basketChecked/${userInfo.id}`}>예약내역</Link></li>
+                            <ul style={ulStyle}>
+                                <li className={'m-lg-3'}><Link to={`/user/mypage/${userInfo.id}`} style={btnStyle}>마이페이지</Link></li>
+                                <li className={'m-lg-3'}><Link to={`/user/basket/${userInfo.id}`} style={btnStyle}>장바구니</Link></li>
+                                <li className={'m-lg-3'}><Link to={`/user/basketChecked/${userInfo.id}`} style={btnStyle}>예약내역</Link></li>
                             </ul>
                         )}
-                            <li className={'m-lg-3'}><Link to={'/logout'}>로그아웃</Link></li>
+                            <li className={'m-lg-3'}><Link to={'/logout'} style={btnStyle}>로그아웃</Link></li>
                         </>
                     ) : (
                         <li>
-                            <span className={'m-lg-3'}><Link to={'/login'}>로그인</Link></span>
-                            <span className={'m-lg-3'}><Link to={'/register'}>회원가입</Link></span>
+                            <span className={'m-lg-3'}><Link to={'/login'} style={btnStyle}>로그인</Link></span>
+                            <span className={'m-lg-3'}><Link to={'/register'} style={btnStyle}>회원가입</Link></span>
                         </li>
                     )}
                 </ul>
